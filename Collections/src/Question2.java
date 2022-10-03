677451438db4606795746c039c56f0b26216e915
AyushJain <ayush.jain3@tothenew.com>
2022-10-03 15:01:20 +0530

    Collections Q2 and Q4 Fixed

--- Collections/src/Question2.java
+++ Collections/src/Question2.java
@@ -1,2 +1,60 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+class Employees implements Comparable<Employees>{
+    private String empFirstName;
+    private String empLastName;
+    private double empSalary;
+    private int empAge;
+
+    Employees(String empFirstName, String empLastName, double empSalary, int empAge){
+        this.empFirstName = empFirstName;
+        this.empLastName = empLastName;
+        this.empSalary = empSalary;
+        this.empAge =empAge;
+    }
+
+    @Override
+    public String toString() {
+        return "Employees{" +
+                "empFirstName='" + empFirstName + '\'' +
+                ", empLastName='" + empLastName + '\'' +
+                ", empSalary=" + empSalary +
+                ", empAge=" + empAge +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Employees employees) {
+        int valueObtainedAfterComparing = this.empFirstName.compareTo(employees.empFirstName);
+        if(valueObtainedAfterComparing==0){
+            return this.empLastName.compareTo(employees.empLastName);
+        }
+        return valueObtainedAfterComparing;
+    }
+}
 public class Question2 {
+    public static void main(String[] args) {
+        Employees e1 = new Employees("Ayush", "Jain", 23434.44, 22);
+        Employees e2 = new Employees("Raj","Bhuva", 31241.24, 23);
+        Employees e3 = new Employees("Utkarsh","Singh", 32739.34, 24);
+        Employees e4 = new Employees("Priyansh","Gupta", 28308,22);
+        Employees e5 = new Employees("Aniket","Agarwal", 77445.78, 21);
+
+        List<Employees> empList  = new ArrayList<>();
+        empList.add(e1);
+        empList.add(e2);
+        empList.add(e3);
+        empList.add(e4);
+        empList.add(e5);
+        Collections.sort(empList);
+
+        for(Employees e : empList){
+            System.out.println(e);
+        }
+    }
+
+
 }
--- Collections/src/Question4.java
+++ Collections/src/Question4.java
@@ -13,6 +13,19 @@ class Employee{
         this.empAge = empAge;
         this.empSalary = empSalary;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return empAge == employee.empAge && empSalary == employee.empSalary && Objects.equals(empName, employee.empName) && Objects.equals(empDesignation, employee.empDesignation);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(empName, empDesignation, empAge, empSalary);
+    }
 }
 public class Question4 {
 
@@ -20,11 +33,13 @@ public class Question4 {
         Employee e1 = new Employee("Ayush", "Software Engineer", 22, 15000);
         Employee e2 = new Employee("Raj", "Software Engineer", 22, 15000);
         Employee e3 = new Employee("Ujjwal", "Devops Engineer", 23, 20000);
+        Employee e4 = new Employee("Ujjwal", "Devops Engineer", 23, 20000);
 
         Map<Employee, Integer> empMap = new HashMap<>();
         empMap.put(e1, e1.empSalary);
         empMap.put(e2, e2.empSalary);
         empMap.put(e3, e3.empSalary);
+        empMap.put(e4, e4.empSalary);
 
         for(Employee e : empMap.keySet()){
             System.out.println(e+": "+empMap.get(e));
