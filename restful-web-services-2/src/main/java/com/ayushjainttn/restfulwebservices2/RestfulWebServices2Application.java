0b7754b5c097739c0f16d297ad8b7b8d4aae8bbe
AyushJain <ayush.jain3@tothenew.com>
2022-10-25 18:26:11 +0530

    restful web services concepts-2 assignment

---
+++ restful-web-services-2/src/main/java/com/ayushjainttn/restfulwebservices2/RestfulWebServices2Application.java
@@ -0,0 +1,13 @@
+package com.ayushjainttn.restfulwebservices2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServices2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServices2Application.class, args);
+	}
+
+}
