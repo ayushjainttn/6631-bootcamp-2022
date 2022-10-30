71ee53d64e7befaee752a639b2a96dda9a88d8c8
AyushJain <ayush.jain3@tothenew.com>
2022-10-30 19:05:14 +0530

    Spring Data JPA and Hibernate Part 2 Assignment JPQL Error Fixed

--- springjpahibernate2/src/main/java/com/ayushjainttn/springjpahibernate2/repository/EmployeeRepository.java
+++ springjpahibernate2/src/main/java/com/ayushjainttn/springjpahibernate2/repository/EmployeeRepository.java
@@ -2,11 +2,55 @@ package com.ayushjainttn.springjpahibernate2.repository;
 
 import com.ayushjainttn.springjpahibernate2.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    @Query("SELECT firstName, lastName FROM Employee WHERE salary > (select AVG(salary) FROM Employee) ORDER BY age ASC, salary DESC")
-    public List<Object[]> findEmployeeBySalaryGreaterThanAverageSalary();
+
+    /*
+    JPQL:
+    1. Display the first name, last name of all employees having salary greater than average salary
+    ordered in ascending by their age and in descending by their salary.
+    2. Update salary of all employees by a salary passed as a parameter whose existing salary is less
+     than the average salary.
+    3. Delete all employees with minimum salary.
+     */
+
+
+    //Querying 1 Query Database using JPQL
+    @Query("SELECT firstName, lastName FROM Employee WHERE salary > :avgsalary ORDER BY age ASC, salary DESC")
+    public List<Object[]> findEmployeeBySalaryGreaterThanAvgSalary(@Param("avgsalary") double avgSalary);
+
+    //Querying Database using JPQL
+    @Query("SELECT AVG(salary) FROM Employee")
+    public List<Object[]> findEmployeeAvgSalary();
+
+    //Querying Database using JPQL
+    @Query("SELECT MIN(salary) FROM Employee")
+    public List<Object[]> findEmployeesWithMinSalary();
+
+    //Querying 2 Query Database using JPQL
+    @Modifying
+    @Query("UPDATE Employee SET salary = salary + :salary WHERE salary < :avgsalary")
+    public void updateEmployeeSalaryIfSalaryLessThanAvgSalary(@Param("salary") double salary, @Param("avgsalary") double avgSalary);
+
+    //Querying 3 Query Database using JPQL
+    @Modifying
+    @Query("DELETE FROM Employee WHERE salary = :minsalary")
+    public void deleteEmployeesWithMinSalary(@Param("minsalary") double minSalary);
+
+
+
+    /*
+    Native SQL Query:
+    1. Display the id, first name, age of all employees where last name ends with "singh"
+    2. Delete all employees with age greater than 45(Should be passed as a parameter)
+     */
+
+    //Querying Database using Native SQL
+
+
 }
--- springjpahibernate2/src/test/java/com/ayushjainttn/springjpahibernate2/Springjpahibernate2ApplicationTests.java
+++ springjpahibernate2/src/test/java/com/ayushjainttn/springjpahibernate2/Springjpahibernate2ApplicationTests.java
@@ -7,6 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -24,16 +26,33 @@ class Springjpahibernate2ApplicationTests {
 	@Test
 	public void testAddEmployees(){
 		Employee emp1 = new Employee();
-		emp1.setFirstName("Himanshi");
-		emp1.setLastName("Khurana");
-		emp1.setAge(24);
-		emp1.setSalary(38790);
+		emp1.setFirstName("Anjali");
+		emp1.setLastName("Singh");
+		emp1.setAge(25);
+		emp1.setSalary(34089);
 		employeeRepository.save(emp1);
 	}
 
 	@Test
 	public void testFindEmployeesBySalaryGreaterThanAvgSalary(){
-		List<Object[]> empList = employeeRepository.findEmployeeBySalaryGreaterThanAverageSalary();
+		List<Object[]> empAvgSalList = employeeRepository.findEmployeeAvgSalary();
+		List<Object[]> empList = employeeRepository.findEmployeeBySalaryGreaterThanAvgSalary((Double)empAvgSalList.get(0)[0]);
 		empList.forEach(employee-> System.out.println(employee[0]+" "+employee[1]));
+	}
+
+	@Test
+	@Transactional
+	@Rollback(false)
+	public void testUpdateEmployeeSalaryIfSalaryLessThanAvgSalary(){
+		List<Object[]> empAvgSalList = employeeRepository.findEmployeeAvgSalary();
+		employeeRepository.updateEmployeeSalaryIfSalaryLessThanAvgSalary(10000, (Double) empAvgSalList.get(0)[0]);
+	}
+
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void testDeleteEmployeesWithMinSalary(){
+		List<Object[]> empMinSalary = employeeRepository.findEmployeesWithMinSalary();
+		employeeRepository.deleteEmployeesWithMinSalary((Double)empMinSalary.get(0)[0]);
 	}
 }
