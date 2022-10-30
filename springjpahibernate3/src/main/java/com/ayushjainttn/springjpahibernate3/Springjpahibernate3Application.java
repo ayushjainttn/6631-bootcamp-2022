ddced655eb15b0af625de34bf502f196f01f3407
AyushJain <ayush.jain3@tothenew.com>
2022-10-31 01:35:30 +0530

    Spring Data JPA and Hibernate Part 3 Assignment

---
+++ springjpahibernate3/src/main/java/com/ayushjainttn/springjpahibernate3/Springjpahibernate3Application.java
@@ -0,0 +1,13 @@
+package com.ayushjainttn.springjpahibernate3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springjpahibernate3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springjpahibernate3Application.class, args);
+	}
+
+}
---
+++ springjpahibernate3/src/test/java/com/ayushjainttn/springjpahibernate3/Springjpahibernate3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayushjainttn.springjpahibernate3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springjpahibernate3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
